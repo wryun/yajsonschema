778e3b23d7af4411a4ae58176a9022e0fd3c61bf
James Haggerty <james.haggerty@gmail.com>
2018-01-26 11:50:27 +1100

    Fix formatting

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// yajsonschema (yet another json schema [something]) is a tool which converts
+// Package yajsonschema (yet another json schema [something]) is a tool which converts
 // a schema written in yaml into a JSON schema (draft 4). i.e. it's primarily
 // an easier way to write json schemas, but you can also use it directly.
 //
